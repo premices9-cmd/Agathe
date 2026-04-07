8a26406ea52df3d1fba4ee18a50dedd19832bcf3
premices9-cmd <premices9@gmail.com>
2026-04-07 20:05:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION DU CACHE ---
 // Change ce numéro (v5.3.3 -> v5.3.4) à chaque fois que tu modifies ton index.html
-const CACHE_NAME = 'mka-shop-v5.3.9'; 
+const CACHE_NAME = 'mka-shop-v5.4.0'; 
 
 const assets = [
   './',
